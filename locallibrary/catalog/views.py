dbd0dc0451b17671aa5379382c45da7f6afd8568
Jalal Sinno <jalal.sinno@dal.ca>
2026-03-03 20:48:16 -0400

    part6 challenge author_list

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -42,3 +42,11 @@ class BookListView(generic.ListView):
 
 class BookDetailView(generic.DetailView):
     model = Book
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 10
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
+    # authors_books = Book.objects.filter(first_name__icontains=model.first_name).filter(last_name__icontains=model.last_name)
