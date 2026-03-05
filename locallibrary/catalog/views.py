b1cbc794e1ed92e04218d005e5d835c8d3c6d646
Jalal Sinno <jalal.sinno@dal.ca>
2026-03-04 20:35:17 -0400

    part 7 complete

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -22,6 +22,11 @@ def index(request):
     # Number of books with and in the title
     num_book_the =  Book.objects.filter(title__icontains='the').count()
 
+    # Number of visits to this view, as counted in the session variable.
+    num_visits = request.session.get('num_visits', 0)
+    num_visits += 1
+    request.session['num_visits'] = num_visits
+
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
@@ -29,6 +34,7 @@ def index(request):
         'num_authors': num_authors,
         'num_genres': num_genres,
         'num_book_the':num_book_the,
+        'num_visits':num_visits,
     }
 
     # Render the HTML template index.html with the data in the context variable
